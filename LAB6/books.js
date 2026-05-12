a9428de2ea829b000f3620b29a304bfc6ebc95b0
Алёна Николаева <alnkaeva@gmail.com>
2026-05-12 20:20:11 +0700

    Книги

---
+++ LAB6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: "Лао-цзы",
+        name: "Дао дэ цзин. Канон о Дао и дэ",
+        publicationYear: 2022,
+        price: 450,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: "Карл Саган",
+        name: "Космос",
+        publicationYear: 2024,
+        price: 890,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Баладжи Касал",
+        name: "Ошибки разумного инвестора",
+        publicationYear: 2025,
+        price: 1200,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: "Джейн Остин",
+        name: "Гордость и предубеждение",
+        publicationYear: 2020,
+        price: 350,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Джордж Оруэлл",
+        name: "1984",
+        publicationYear: 2021,
+        price: 520,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Рэй Брэдбери",
+        name: "451° по Фаренгейту",
+        publicationYear: 2019,
+        price: 480,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: "Михаил Булгаков",
+        name: "Мастер и Маргарита",
+        publicationYear: 2023,
+        price: 650,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Эрих Мария Ремарк",
+        name: "Три товарища",
+        publicationYear: 2020,
+        price: 590,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: "Антуан де Сент-Экзюпери",
+        name: "Маленький принц",
+        publicationYear: 2022,
+        price: 300,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: "Фёдор Достоевский",
+        name: "Преступление и наказание",
+        publicationYear: 2021,
+        price: 700,
+        bestseller: true,
+        inStock: true
+    }
+]
